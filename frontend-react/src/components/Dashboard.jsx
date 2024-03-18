1868076123f0662480b7d4b40c98e5a303c4b7d3
herviansyahreza <reza.herviansyah99@gmail.com>
2024-03-18 21:55:55 +0700

    1

--- frontend-react/src/components/Dashboard.jsx
+++ frontend-react/src/components/Dashboard.jsx
@@ -1,11 +1,15 @@
 import React from 'react';
 // import { Link } from 'react-router-dom';
 import DashboardStatsGrid from './DashboardStatGrid';
+import DashboardChart from './DashboardChart';
 
 export default function Dashboard() {
     return (
-        <div className='flex gap-4'>
+        <div className='flex gap-4 flex-col'>
             <DashboardStatsGrid />
+            <div className="flex flex-row gap-4 m-10">
+                <DashboardChart />
+            </div>
         </div>
     );
 }

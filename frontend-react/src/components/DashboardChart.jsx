3b8e8be4d844749fbe0d4fc7fa68ccbe89d045e0
herviansyahreza <reza.herviansyah99@gmail.com>
2024-06-21 14:33:36 +0700

    Update DashboardChart.jsx

--- frontend-react/src/components/DashboardChart.jsx
+++ frontend-react/src/components/DashboardChart.jsx
@@ -91,33 +91,64 @@ const data = [
 
 export default function TicketReportChart() {
 	return (
-		<div className="h-[22rem] bg-white p-4 rounded-sm border border-gray-200 flex flex-col flex-1">
-			<strong className="text-gray-700 font-medium">Ticket Report</strong>
-			<div className="mt-3 w-full flex-1 text-xs">
-				<ResponsiveContainer width="100%" height="100%">
-					<BarChart
-						width={500}
-						height={300}
-						data={data}
-						margin={{
-							top: 20,
-							right: 10,
-							left: -10,
-							bottom: 0
-						}}
-					>
-						<CartesianGrid strokeDasharray="3 3 0 0" vertical={false} />
-						<XAxis dataKey="name" />
-						<YAxis />
-						<Tooltip />
-						<Legend />
-						<Bar dataKey="Urgent" fill="#171717" />
-						<Bar dataKey="High" fill="#b91c1c" />
-						<Bar dataKey="Medium" fill="#fbbf24" />
-						<Bar dataKey="Low" fill="#a3e635" />
-					</BarChart>
-				</ResponsiveContainer>
+		<div className="h-screen overflow-auto bg-gray-100 p-4">
+			<div className="h-[22rem] w-[72rem] bg-white p-4 rounded-sm border border-gray-200 flex flex-col flex-1 mb-8">
+				<strong className="text-gray-700 font-medium">Ticket Report by Month</strong>
+				<div className="mt-3 w-full flex-1 text-xs">
+					<ResponsiveContainer width="100%" height="100%">
+						<BarChart
+							width={500}
+							height={300}
+							data={data}
+							margin={{
+								top: 20,
+								right: 10,
+								left: -10,
+								bottom: 0
+							}}
+						>
+							<CartesianGrid strokeDasharray="3 3 0 0" vertical={false} />
+							<XAxis dataKey="name" />
+							<YAxis />
+							<Tooltip />
+							<Legend />
+							<Bar dataKey="Urgent" fill="#171717" />
+							<Bar dataKey="High" fill="#b91c1c" />
+							<Bar dataKey="Medium" fill="#fbbf24" />
+							<Bar dataKey="Low" fill="#a3e635" />
+						</BarChart>
+					</ResponsiveContainer>
+				</div>
+			</div>
+
+			<div className="h-[22rem] w-[72rem] bg-white p-4 rounded-sm border border-gray-200 flex flex-col flex-1">
+				<strong className="text-gray-700 font-medium">Ticket Report by Asset</strong>
+				<div className="mt-3 w-full flex-1 text-xs">
+					<ResponsiveContainer width="100%" height="100%">
+						<BarChart
+							width={500}
+							height={300}
+							data={data}
+							margin={{
+								top: 20,
+								right: 10,
+								left: -10,
+								bottom: 0
+							}}
+						>
+							<CartesianGrid strokeDasharray="3 3 0 0" vertical={false} />
+							<XAxis dataKey="name" />
+							<YAxis />
+							<Tooltip />
+							<Legend />
+							<Bar dataKey="Urgent" fill="#171717" />
+							<Bar dataKey="High" fill="#b91c1c" />
+							<Bar dataKey="Medium" fill="#fbbf24" />
+							<Bar dataKey="Low" fill="#a3e635" />
+						</BarChart>
+					</ResponsiveContainer>
+				</div>
 			</div>
 		</div>
-	)
+	);
 }
